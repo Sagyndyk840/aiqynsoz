c77e9724d92312d03a6493973025f6bb8dbc3ce9
Sagyndyk <sagindik2003@gmail.com>
2025-07-19 17:59:47 +0500

    - Add: tailwind css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '@app/styles/index.css'
 import { RouterProvider } from 'react-router-dom'
+import '@/app/styles/index.css'
 import { router } from '@app/App'
 
 const container = document.getElementById('root')
